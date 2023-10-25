0e5a8eee785235d6b2a867df651e107626825831
Sadit Shah <saditshah@gmail.com>
2023-10-25 22:03:36 +0530

    themeswitcher

--- app/components/toggleSwitch/toggleSwitch.js
+++ app/components/toggleSwitch/toggleSwitch.js
@@ -1,24 +1,25 @@
 'use client'
 import React, { useState } from 'react';
+import { BsSun, BsMoon } from 'react-icons/bs'
+
 
 function ToggleSwitch() {
-  const [isChecked, setIsChecked] = useState(false);
+  const [theme, setTheme] = useState('light')
 
   const handleToggle = () => {
-    setIsChecked(!isChecked);
-    if (isChecked)
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+    console.log(theme)
+    if (theme === 'dark')
       document.getElementsByTagName('html')[0].setAttribute('data-theme', 'light')
     else
       document.getElementsByTagName('html')[0].setAttribute('data-theme', 'dark')
   };
 
   return (
-    <label className={`relative inline-block w-16 h-7 rounded-3xl ${isChecked ? 'bg-blue-500' : 'bg-gray-400'}`}>
-      <input type="checkbox" className="hidden" checked={isChecked} onChange={handleToggle} />
-      <span className="absolute cursor-pointer top-0 left-0 right-0 bottom-0 transition-transform rounded-[34px]">
-        <span className={`absolute cursor-pointer w-5 h-5 bg-white rounded-full transition-transform ${isChecked ? 'transform translate-x-8' : ''}`} style={{ left: '4px', bottom: '4px', boxShadow: isChecked ? '0 0 1px #2196F3' : 'none' }}></span>
-      </span>
-    </label>
+    <div className='cursor-pointer text-2xl' onClick={handleToggle}>
+      <BsSun className={`${theme==='light'?'visible':'hidden'}`}/>
+      <BsMoon className={`${theme==='dark'?'visible':'hidden'}`}/>
+    </div>
   );
 }
 
